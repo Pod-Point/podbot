762fa2a6e2ac4efaf00b7ab6cf2418d7fc6ebec9
Ben Speakman <ben.speakman@pod-point.com>
2016-07-15 15:24:33 +0100

    Codeship webhook

---
+++ src/modules/codeship.js
@@ -0,0 +1,56 @@
+import Base from './base';
+
+class Codeship extends Base {
+
+    /**
+     * Register any webhooks to be listened for
+     *
+     * @param  {[type]} bot
+     * @param  {[type]} webserver
+     * @return {void}
+     */
+    webhooks(bot, webserver) {
+        webserver.post('/codeship', (req, res) => {
+
+            console.log(req.body);
+
+            let data = req.body.build;
+
+            if (data.status == 'error' && data.branch == 'master') {
+
+                let message = {
+
+                    channel: '#bottesting',
+                    attachments: [
+                        {
+                            fallback: `Codeship build for ${data.branch} branch on ${data.project_name} has failed.`,
+                            color: 'danger',
+                            title: 'Codeship build failed',
+                            title_link: data.build_url,
+                            text: data.message,
+                            fields: [
+                                {
+                                    title: 'Project',
+                                    value: data.project_name,
+                                    short: true
+                                },
+                                {
+                                    title: 'Branch',
+                                    value: data.branch,
+                                    short: true
+                                }
+                            ]
+                        }
+                    ]
+                };
+
+                bot.say(message);
+
+            }
+
+            res.send('OK');
+        });
+    }
+}
+
+export default Codeship;
