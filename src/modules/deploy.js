50129d5098eec5919a775e6c68ac2a0626370bc5
Ben Speakman <ben.speakman@pod-point.com>
2016-07-15 14:55:54 +0100

    Use interactive messages

--- src/modules/deploy.js
+++ src/modules/deploy.js
@@ -11,7 +11,6 @@ class Deploy extends Base {
      * @return {void}
      */
     messageListeners(controller) {
-
         controller.hears('deploy (.*) with comment (.*)', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
 
             let name = message.match[1];
@@ -23,8 +22,34 @@ class Deploy extends Base {
 
             if (app) {
 
-                let opsworks = new Opsworks(bot.reply, message);
-                opsworks.deploy(app, comment);
+                bot.reply(message, {
+                    attachments: [
+                        {
+                            title: `Deploy ${app.name} to all stacks`,
+                            text: 'Are you sure?',
+                            callback_id: 'deploy',
+                            attachment_type: 'default',
+                            actions: [
+                                {
+                                    name: 'yes',
+                                    text: ':shipit:',
+                                    value: JSON.stringify({
+                                        name: name,
+                                        comment: comment
+                                    }),
+                                    style: 'primary',
+                                    type: 'button'
+                                },
+                                {
+                                    name: 'no',
+                                    text: ':thumbsdown:',
+                                    style: 'danger',
+                                    type: 'button'
+                                }
+                            ]
+                        }
+                    ]
+                });
 
             } else {
 
@@ -38,9 +63,54 @@ class Deploy extends Base {
                 });
 
             }
-
         });
+    }
 
+    /**
+     * Register any message callbacks to be listened for
+     *
+     * @param  {[type]} message
+     * @return {void}
+     */
+    callbacks(bot, message) {
+
+        if (message.callback_id == 'deploy') {
+
+            let action = message.actions[0];
+
+            if (action.name == 'yes') {
+
+                let data = JSON.parse(action.value);
+                let app = Apps.find((app) => {
+                    return app.name == data.name;
+                });
+
+                if (app) {
+
+                    let opsworks = new Opsworks(bot.replyInteractive, message);
+                    opsworks.deploy(app, data.comment);
+
+                } else {
+
+                    bot.replyInteractive(message, {
+                        attachments: [
+                            {
+                                color: 'warning',
+                                title: `Sorry I dont know how to deploy ${data.name} :disappointed:`
+                            }
+                        ]
+                    });
+
+                }
+
+            } else {
+
+                bot.replyInteractive(message, {
+                    attachments: [{}]
+                });
+
+            }
+        }
     }
 }
 
