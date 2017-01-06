e50105efc8ec92a5833328612ed9e1cd890b20f8
Ben Speakman <ben.speakman@pod-point.com>
2017-01-06 09:55:15 +0000

    Update podbot cron messages

--- src/modules/messages.ts
+++ src/modules/messages.ts
@@ -11,20 +11,11 @@ export default class Messages {
      */
     public cronjobs(bot: SlackBot): void {
 
-        new CronJob('00 45 09 * * 1-5', () => {
+        new CronJob('00 15 09 * * 1-5', () => {
 
             bot.say({
                 channel: Config.get<string>('channels.software.code'),
-                text: 'Morning team, don\'t forget to sign into Jell and update your standup status before 10am :+1:'
-            });
-
-        }, null, true, 'Europe/London');
-
-        new CronJob('00 45 12 * * 4', () => {
-
-            bot.say({
-                channel: Config.get<string>('channels.software.code'),
-                text: 'Don\'t forget, we\'ve got story planning at 2pm today so don\'t leave lunch too late!'
+                text: 'Morning team, don\'t forget we have a standup at 9:30am :+1:'
             });
 
         }, null, true, 'Europe/London');
