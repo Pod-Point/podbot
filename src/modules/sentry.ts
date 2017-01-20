57b95f87163a4d87f58f09146654a198a8c2b3a2
Ben Speakman <ben.speakman@pod-point.com>
2017-01-20 11:42:35 +0000

    sentry webhook to add trello cards

---
+++ src/modules/sentry.ts
@@ -0,0 +1,37 @@
+import Trello from 'node-trello';
+
+export default class Sentry {
+
+    /**
+     * Register any webhooks to be listened for
+     *
+     * @param  {SlackBot} bot
+     * @param  {WebServer} webserver
+     * @return {void}
+     */
+    public webhooks(bot: SlackBot, webserver: WebServer): void {
+        webserver.post('/sentry', (req, res) => {
+
+            const trello = new Trello(process.env.GITHUB_TOKEN, process.env.GITHUB_TOKEN);
+            const data: SentryWebhook = req.body;
+
+            const trelloData = {
+                idList: 1,
+                name: data.message,
+                desc: data.message,
+                urlSource: data.url
+            };
+
+            trello.post('1/cards', trelloData, (err: any, response: any) => {
+                console.log(response);
+            });
+
+            res.send('OK');
+        });
+    }
+}
+
+interface SentryWebhook {
+    message: string;
+    url: string;
+}
