a4b3bb8c104a9a07883db2d8fd44e9062f2ba656
Joe Hall <joe.hall@pod-point.com>
2017-03-22 11:22:58 +0000

    Moved log & file-stamp classes to helpers directory

---
+++ src/helpers/file-stamp.ts
@@ -0,0 +1,29 @@
+export default class FileStamp {
+
+    /**
+     * Create a date and time string for file names
+     *
+     * @return string
+     * 
+     */
+    public dateTime(): string {
+        const currentDate: Date = new Date();
+        return currentDate.getFullYear() + '-' +
+               this.addLeadingZero(currentDate.getMonth() + 1) + '-' +
+               this.addLeadingZero(currentDate.getDate()) + '--' +
+               this.addLeadingZero(currentDate.getHours()) + '-' +
+               this.addLeadingZero(currentDate.getMinutes()) + '-' +
+               this.addLeadingZero(currentDate.getSeconds());
+    }
+
+    /**
+     * Adds a leading zero to single digit date/time values, to assist when sorting file names using these
+     * 
+     * @param {number} input
+     * 
+     */
+    private addLeadingZero(input: number): string {
+        return ('0' + input).slice(-2);
+    }
+
+}
---
+++ src/helpers/log.spec.ts
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import Log from './log';
+
+describe('Log', () => {
+
+    describe('Create log file', () => {
+
+        it('saves a log file', () => {
+
+            const logFileName: string = 'test';
+            const logContent: string = 'Dummy log contents\nand some more more';
+            const log: Log = new Log();
+
+            return log.createLogFile(logFileName, logContent).then((response) => {
+                expect(response).to.equal(log.formatLogMsg('Log file created'));
+            });
+
+        });
+
+    });
+
+    describe('Format a log message', () => {
+
+        it('formats a log message into a one-line string with line break', () => {
+
+            const logMessage = {
+                'Some': {
+                    'dummy': {
+                        'content': { }
+                    }
+                }
+            };
+            const log: Log = new Log();
+            const response = log.formatLogMsg(logMessage);
+            expect(response).to.equal('{"Some":{"dummy":{"content":{}}}}\n');
+
+        });
+
+    });
+
+});
